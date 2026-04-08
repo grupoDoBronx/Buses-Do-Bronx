e1f96b8e0fd2fa1ebe855e033b7475960112b106
javier2811 <javier.torres2402@alumnos.ubiobio.cl>
2026-04-07 23:13:33 -0400

    Bosquejo de clase bus

---
+++ Bus.java
@@ -0,0 +1,15 @@
+package Figura2;
+
+public class Bus {
+    private String patente;
+    private String marca;
+    private String modelo;
+    private int nroAsientos;
+
+    public Bus(String patente, String marca, String modelo, int nroAsientos) {
+        this.patente= patente;
+        this.marca= marca;
+        this.modelo=modelo;
+        this.nroAsientos= nroAsientos;
+    }
+}
