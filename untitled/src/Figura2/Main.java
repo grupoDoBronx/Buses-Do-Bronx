8ff483bee7c64ca4c7c2f1ae23085c5675825593
HerreroNovato <toppharold460@gmail.com>
2026-04-13 10:15:21 -0400

    menu

--- untitled/src/Figura2/Main.java
+++ untitled/src/Figura2/Main.java
@@ -48,9 +48,60 @@ public class Main {
 
     }
     private void createCliente(){
-        System.out.println();
+        String rutcliente;
+        String pasaportecliente;
+        boolean existecliente = false;
+        System.out.println("Ingrese el tipo de identificador del cliente: \n1. Rut\n2. Pasaporte");
+        int identificador = sc.nextInt();
+        if (identificador == 1){
+            System.out.println("Ingrese el rut del cliente: ");
+            rutcliente = sc.nextLine();
+        } else if (identificador == 2) {
+            System.out.println("Ingrese el pasaporte del cliente: ");
+            pasaportecliente = sc.nextLine();
+        }else{
+            return;
+        }
+        for (int i = 0; i < ; i++) {
+            if(String )
+        }
+        if (existecliente = false){
+            System.out.println("Ingrese si es Sr.(1) o Sra.(2):");
+            int srsra = sc.nextInt();
+            System.out.println("Ingrese los nombres del  cliente:");
+            String nombres = sc.nextLine();
+            System.out.println("Ingrese el Apellido Paterno del cliente:");
+            String apepaterno = sc.next();
+            System.out.println("Ingrese el Apellido Materno del cliente:");
+            String apematerno = sc.next();
+            System.out.println("Ingrese el numero telefonico del cliente:");
+            String telefonoCliente = sc.nextLine();
+            System.out.println("Ingrese el email del cliente:");
+            String emailCliente = sc.nextLine();
+
+
+            System.out.println("   ...:::: Crear un nuevo Cliente ::::....");
+            System.out.println("Rut[1] o Pasaporte[2] : " + identificador);
+            if (identificador == 1){
+                System.out.println("R.U.T : " + rutcliente);
+            } else if (identificador == 2) {
+                System.out.println("Pasaporte Cliente : " + pasaportecliente);
+            }
+            System.out.println("Sr.[1] o Sra. [2] : " + srsra);
+            System.out.println("Nombres : " + nombres);
+            System.out.println("Apellido Paterno : " + apepaterno);
+            System.out.println("Apellido Materno : " + apematerno);
+            System.out.println("Telefono movil : " + telefonoCliente);
+            System.out.println("Email : " + emailCliente +"\n");
+            System.out.println("...:::: Cliente guardado exitosamente ::::....");
+        }else{
+            System.out.println("Ya existe un cliente con esta identificacion");
+            return;
+        }
+
     }
     private void createBus(){
+        System.out.println("Ingrese la patente del bus: ");
 
     }
     private void createViaje(){
@@ -66,6 +117,7 @@ public class Main {
     }
     private void listVentas(){
         for (int i = 0; i < ; i++) {
+
 
         }
     }
