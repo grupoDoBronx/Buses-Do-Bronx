54525f8b3437e32e6f1203b3364c6a505e3758c1
Diegosw12 <diego.jara2501@alumnos.ubiobio.cl>
2026-04-25 21:10:53 -0400

    .

--- untitled/src/Figura2/Bus.java
+++ untitled/src/Figura2/Bus.java
@@ -1,15 +1,10 @@
-package Figura2.untitled.src.Figura2;
-
 public class Bus {
     private String patente;
     private String marca;
     private String modelo;
     private int nroAsientos;
-
-    public Bus(String patente, String marca, String modelo, int nroAsientos) {
         this.patente= patente;
         this.marca= marca;
         this.modelo=modelo;
-        this.nroAsientos= nroAsientos;
     }
 }
