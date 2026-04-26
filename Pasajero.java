05917c896584f4a079e760f7f16184ed663cfb04
javier2811 <javier.torres2402@alumnos.ubiobio.cl>
2026-04-26 02:08:27 -0400

    Se construyó objeto "pasajero" y getter y setters del contacto del usuario.

--- Pasajero.java
+++ Pasajero.java
@@ -1,5 +1,23 @@
 package Figura2;
 
 public class Pasajero extends Persona {
-    private Nombre
+    private Nombre nomContacto;
+    private String fonoContacto;
+
+    public Pasajero(IdPersona id, Nombre nombreCompleto, String telefono, Nombre nomContacto, String fonoContacto){
+        super (id, nombreCompleto);
+        setTelefono(telefono);
+        this.nomContacto= nomContacto;
+        this.fonoContacto = fonoContacto;
+    }
+
+    public Nombre getNomContacto() {
+        return nomContacto;
+    }
+
+    public void setNomContacto(Nombre nom){
+        this.nomContacto = nom;
+    }
+
+
 }
