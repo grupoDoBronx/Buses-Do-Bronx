1de46971c41f35654538b0ef65d1d4a334731bbe
HerreroNovato <toppharold460@gmail.com>
2026-04-25 21:50:36 -0400

    la cague en el merch y no subi esto

--- untitled/src/Figura2/SistemaVentaPasajes.java
+++ untitled/src/Figura2/SistemaVentaPasajes.java
@@ -1,17 +1,23 @@
 package Figura2;
 
+import Figura1.IdPersona;
+import Figura1.Nombre;
 import enums.TipoDocumento;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class SistemaVentaPasajes {
-    public boolean createCliente (IdPersona id, Nombre nom, String fono, String email){
+    Main main = new Main();
+
+    public boolean createCliente (IdPersona id, Nombre nombre, String fono, String email){
+        findCliente()
         return true;
     }
 
 
     public boolean createPasajero (IdPersona id, Nombre nom, String fono, Nombre nomContacto, String fonoContacto){
+
         return true;
     }
 
@@ -22,11 +28,35 @@ public class SistemaVentaPasajes {
 
 
     public boolean createViaje (LocalDate fecha, LocalTime hora, int precio, String patBus){
+
         return true;
     }
 
 
     public boolean iniciaVenta (String idDoc, TipoDocumento tipo, LocalDate fechaVenta, IdPersona idCliente){
+        return true;
+    }
+    public String[][] getHorariosDisponibles(LocalDate fechaViaje){
+
+        return horariosDisponibles();
+    }
+    public String[][] listAsientosDeViaje(LocalDate fecha,LocalTime hora,String patBus){
+        for (int i = 0; i < ; i++) {
+
+        }
+    }
+    public int getMontoVenta(String idDocumento, TipoDocumento tipo){
+        int montoVenta = 0;
+        return montoVenta;
+    }
+    public String getNombrePasajero(IdPersona idPasajero){
+
+        String nombrePasajero =
+        return nombrePasajero
+    }
+    public boolean vendePasaje(String idDoc,LocalDate fecha,LocalTime hora,String patBus,int asiento){
+        findVenta();
+        findCliente();
         return true;
     }
     public String [][] listVentas(){
@@ -35,13 +65,23 @@ public class SistemaVentaPasajes {
     public String [][] listViajes(){
         return listViajes();
     }
-    public String getHorarios{
-        return horarios;
+    public String [][] listPasajeros(LocalDate fecha,LocalTime hora, String patBus){
+
+    }
+    private Cliente findCliente(IdPersona id){
+        return findCliente(id);
+    }
+    private Venta findVenta(String idDocumento,TipoDocumento tipoDocumento){
+
+    }
+    private Bus findBus(String patente){
+
+    }
+    private Viaje findViaje(String fecha, String hora, String patenteBus){
+
+    }
+    private Pasajero findPasajero(IdPersona idPersona){
+
     }
 
 
-
-
-
-
-}
