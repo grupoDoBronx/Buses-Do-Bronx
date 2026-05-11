7b6be10c6a8a9bcc083f5c77f3aa0e4b44ece702
sebaubb25 <sebastian.jara2501@alumnos.ubiobio.cl>
2026-05-11 15:30:26 -0400

    termine Terminal

--- untitled/src/modelo/Terminal.java
+++ untitled/src/modelo/Terminal.java
@@ -1,17 +1,24 @@
 package modelo;
 
-import utilidades.Direccion;
+import utilidades.*;
+
+import java.util.ArrayList;
 
 public class Terminal {
     private String nombre;
     private Direccion direccion;
+    //agrego 2 array list para corregir el error
+    private ArrayList<Viaje> llegadas;
+    private ArrayList<Viaje> salidas;
 
-    public Terminal(Direccion direccion, String nombre) {
-        this.direccion = direccion;
-        this.nombre = nombre;
+    public Terminal(String nombre,Direccion direccion){
+        this.nombre=nombre;
+        this.direccion=direccion;
+
+        llegadas=new ArrayList<>();
+        salidas=new ArrayList<>();
     }
-
-    public String getNombre() {
+    public String getNombre(){
         return nombre;
     }
 
@@ -21,5 +28,19 @@ public class Terminal {
 
     public void setDireccion(Direccion direccion) {
         this.direccion = direccion;
+    }
+
+    public void addLlegada(Viaje viaje){
+        llegadas.add(viaje);
+    }
+    public void addSalida(Viaje viaje){
+        salidas.add(viaje);
+    }
+    public Viaje[] getLlegadas(){
+        return llegadas.toArray(new Viaje[0]);
+    }
+    public Viaje[] getSalidas(){
+        return salidas.toArray(new Viaje[0]);
+
     }
 }
